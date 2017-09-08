16ca631a16f4948f5d7092c9c3808e8c402737e9
emilandreas <emilandreaslund@gmail.com>
2017-09-08 14:05:24 +0200

    test ssh

--- src/OpenAiRobot/agent.py
+++ src/OpenAiRobot/agent.py
@@ -7,8 +7,10 @@ class Agent:
         #the state is given by the last n (4?) frames, and saved in memory
         self.memory = np.empty(self.network.get_input_shape())
         self.memory_size, _, _ = self.network.get_input_shape()
-    def 
+    def
     def __update_memory(self, obs):
         for i in reversed(range(1, self.memory_size)):
             self.memory[i, :, :] = self.memory[i-1, :, :]
         self.memory[0, :, :] = obs
+    def testssh(self):
+        a = 2
